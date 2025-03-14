e4e46064c28246d881c4b67b4950f1d8a7f52871
Your Name <you@example.com>
2025-03-14 10:39:46 +0100

    Script for initializing a room

---
+++ initialize_thm_room.sh
@@ -0,0 +1,49 @@
+#! /bin/bash
+
+# Initialize variables
+room=""
+ip=""
+
+# Function to display usage
+usage() {
+    echo "Usage: $0 --room <thm_room> --ip <target_ip>"
+    exit 1
+}
+
+# Parse arguments
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --room) room="$2"; shift ;;
+        --ip) ip="$2"; shift ;;
+        *) usage ;;
+    esac
+    shift
+done
+
+# Check if all required arguments are provided
+if [ -z "$room" ] || [ -z "$ip" ]; then
+    usage
+fi
+
+# Vérifiez si le dossier existe
+if [ ! -d "$room" ]; then
+    mkdir $room;
+fi
+
+# init.sh folder
+echo "#! /bin/bash" > $room/init.sh
+echo "TARGET=$ip">>$room/init.sh
+echo "ROCKYOU_WL=/usr/share/wordlists/SecLists/Passwords/Leaked-Databases/rockyou.txt">>$room/init.sh
+echo "DIR_WL=/usr/share/wordlists/SecLists/Discovery/Web-Content/directory-list-2.3-big.txt">>$room/init.sh
+chmod +x $room/init.sh 
+
+
+# scans folder
+if [ ! -d "$room/scans" ]; then
+    mkdir $room/scans;
+fi
+
+# webserver folder
+if [ ! -d "$room/webserver" ]; then
+	mkdir $room/webserver;
+fi
