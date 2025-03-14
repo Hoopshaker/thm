31acf0151a0daa0ec6f36cffc2131abc70900204
Hoopshaker <you@example.com>
2025-03-14 11:39:15 +0100

    Modifying initialization script to add ignore file in created room folders

--- initialize_thm_room.sh
+++ initialize_thm_room.sh
@@ -27,16 +27,32 @@ fi
 
 # Vérifiez si le dossier existe
 if [ ! -d "$room" ]; then
-    mkdir $room;
+	echo "Creating folder $room"
+	mkdir $room;
+else
+	echo "Folder $room already exists"
 fi
 
 # init.sh folder
+if [ -f "$room/init.sh" ]; then
+	echo "Updating init.sh file"
+else
+	echo "Creating init.sh file"
+fi
+
 echo "#! /bin/bash" > $room/init.sh
 echo "TARGET=$ip">>$room/init.sh
 echo "ROCKYOU_WL=/usr/share/wordlists/SecLists/Passwords/Leaked-Databases/rockyou.txt">>$room/init.sh
 echo "DIR_WL=/usr/share/wordlists/SecLists/Discovery/Web-Content/directory-list-2.3-big.txt">>$room/init.sh
 chmod +x $room/init.sh 
 
+# adding .gitignore file to folder
+if [ -f "$room/.gitignore" ]; then
+	echo ".gitignore file exists"
+else
+	echo "Creating .gitignore file"
+	echo "init.sh" > $room/.gitignore
+fi
 
 # scans folder
 if [ ! -d "$room/scans" ]; then
